d04cda33ae43f1ca9a40faf08ffaea7d451b0d7b
xusongqi <xusongqi@foxmail.com>
2014-06-18 13:24:13 +0800

    使用到的系统头文件

---
+++ client/include/ftp_client_include.h
@@ -0,0 +1,27 @@
+/* 
+* Author:		xusongqi@live.com
+* 
+* Created Time: 2014年06月17日 星期二 18时08分17秒
+* 
+* FileName:     ftp_client_include.h
+* 
+* Description:  
+*
+*/
+
+#ifndef _FTP_CLIENT_INCLUDE_
+#define _FTP_CLIENT_INCLUDE_
+
+#include <libxml/tree.h>
+#include <libxml/parser.h>
+#include <netinet/in.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+//#include <openssl/md5.h>
+#include <sys/sendfile.h>
+#include <sys/socket.h>
+#include <sys/types.h>
+#include <unistd.h>
+
+#endif
